1eb261964e6f87b8d3991a3ec6cd08101f28f1aa
Ahmadshoh Nasrullozoda <a.nasrullozoda@AD.ITS-STUTTGART.LOGODIDACT.NET>
2022-03-24 11:56:30 +0100

    init

---
+++ layout/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet/less" type="text/css" href="../styles/main.less" />
+<script src="https://cdn.jsdelivr.net/npm/less@4" ></script>
+</head>
+<body>
+
+   <header>
+        <nav>
+            <ul>
+                <li>Home</li>
+                <li>Produkte</li>
+                <li>Dokumentation</li>
+                <li>Über uns</li>
+            </ul>
+        </nav>
+   </header>
---
+++ views/index.php
@@ -0,0 +1,7 @@
+<?php include '../layout/header.php' ?>
+<header class="header">
+    <img class="header-image" src="images/header.jpg" alt="" style="height:100px">
+<header>
+
+
+<?php include '../layout/footer.php' ?>
